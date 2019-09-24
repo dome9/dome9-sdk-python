481218af14168a800dc06e5e876a02fb001924a7
Erez Bodner <erezbo@dome9.com>
2019-09-24 17:03:41 +0300

    Update to Python3
    - Changed to Python3 code

--- dome9_sdk_python/__init__.py
+++ dome9_sdk_python/__init__.py
@@ -1,9 +1,19 @@
 from ._version import __version__
-from .dome9_api_sdk import Dome9ApiSDK
-from .dome9_api_client import Dome9ApiClient
+from .dome9_api_exceptions import Dome9APIException
+from .dome9_api_sdk import Dome9APISDK
+from .dome9_api_client import Dome9APIClient
+from .dome9_api_consts import Protocols, Regions, OperationModes, ProtectionModes, CloudAccountTypes, NewGroupBehaviors, Vendors
 
 
 __all__ = [
-           'Dome9ApiSDK',
-           'Dome9ApiClient'
-]
+           'Dome9APIException',
+           'Dome9APISDK',
+           'Dome9APIClient',
+           'Protocols',
+           'Regions',
+           'OperationModes',
+           'ProtectionModes',
+           'CloudAccountTypes',
+           'NewGroupBehaviors',
+           'Vendors'
+           ]
