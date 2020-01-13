2ebaaebc52621fc201ae182158f80cffe0470ba7
Ayal <ayalg@checkpoint.com>
2020-01-13 18:13:11 +0200

    CR Fix 1

--- dome9/base_dataclass.py
+++ dome9/base_dataclass.py
@@ -10,16 +10,5 @@ from dome9.utils import Utils
 class BaseDataclassRequest:
 
 	def load(self, skip_empty=True):
-		return BaseDataclassRequest.convert_keys_to_lowerCase(dict_to_convert=self.to_dict(), skip_empty=skip_empty)
+		return Utils.convert_keys_to_camel_case(dict_to_convert=self.to_dict(), skip_empty=skip_empty)
 
-	@staticmethod
-	def convert_keys_to_lowerCase(dict_to_convert: dict, skip_empty: bool):
-		payload = {}
-		for key, value in dict_to_convert.items():
-			if isinstance(value, dict):
-				value = BaseDataclassRequest.convert_keys_to_lowerCase(dict_to_convert=value, skip_empty=skip_empty)
-			# discard unset variables (None) recursively
-			if not (skip_empty and value is None):
-				payload[Utils.convert_to_camel_case(key)] = value
-
-		return payload
