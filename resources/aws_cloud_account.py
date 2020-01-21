26380e971d790ae4e64d47b803fd4f6984932100
erez-dome9 <erez@dome9.com>
2020-01-21 19:23:04 +0200

    AWS cloud account docstring alignment (#31)

--- resources/aws_cloud_account.py
+++ resources/aws_cloud_account.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from loguru import logger
 
@@ -10,33 +10,34 @@ from dome9.exceptions import UnsupportedCloudAccountCredentialsBasedType, Unsupp
 
 
 class AwsCloudAccountConsts(Enum):
-	MAIN_ROUTE = 'CloudAccounts'
-	REGION_CONFIG_ROUTE = 'region-conf'
-	ORGANIZATIONAL_UNIT_ROUTE = 'organizationalUnit'
-	NAME_ROUTE = 'name'
-	CREDENTIALS_ROUTE = 'credentials'
+	CLOUD_ACCOUNTS = 'CloudAccounts'
+	REGION_CONFIG = 'region-conf'
+	ORGANIZATIONAL_UNIT = 'organizationalUnit'
+	NAME = 'name'
+	CREDENTIALS = 'credentials'
 
 
 class AwsCloudAccountCredentialsBasedType(Enum):
-	USER_BASED_TYPE = 'UserBased'
-	ROLE_BASED_TYPE = 'RoleBased'
+	USER_BASED = 'UserBased'
+	ROLE_BASED = 'RoleBased'
 
 
 @dataclass
-class AwsCloudAccountCredentials:
-	"""AWS cloud account credentials
+class CloudAccountCredentials:
+	"""The information needed for Dome9 System in order to connect to the AWS cloud account
 
-		Args:
-			arn (str): (Required) AWS Role ARN (to be assumed by Dome9)
-			secret (str): (Required) The AWS role External ID (Dome9 will have to use this secret in order to assume the role)
-			type (str): (Required) The cloud account onboarding method. Set to "RoleBased".
-			api_key (str): (Optional) aws cloud account apiKey.
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountcredentialsviewmodel
+	:param arn: [Required] AWS Role ARN (to be assumed by Dome9 System)
+	:type  arn: str
+	:param secret: [Required] The AWS role External ID (Dome9 System will have to use this secret in order to assume the role)
+	:type  secret: str
+	:param type: [Required] The cloud account onbiarding method. Should be set to "RoleBased" as other methods are deprecated
+	:type  type str
 
 	"""
 	arn: str
 	secret: str
-	type: str = AwsCloudAccountCredentialsBasedType.ROLE_BASED_TYPE.value
-	api_key: str = None
+	type: str = AwsCloudAccountCredentialsBasedType.ROLE_BASED.value
 
 	@logger.catch(reraise=True)
 	def __post_init__(self):
@@ -46,28 +47,34 @@ class AwsCloudAccountCredentials:
 
 
 @dataclass
-class AwsCloudAccountRequest(BaseDataclassRequest):
-	"""AWS cloud account request
+class CloudAccount(BaseDataclassRequest):
+	"""The new AWS account data
 
-		Args:
-			name (str): (Required) The name of AWS account in Dome9
-			credentials (AwsCloudAccountCredentials): (Required) The information needed for Dome9 System in order to connect to the AWS cloud account
-			organizational_unit_id (str): (Optional) The Organizational Unit that this cloud account will be attached to
+	:link https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+	:param name: The cloud account name
+	:type name: str
+	:param credentials: [Required] The information needed for Dome9 System in order to connect to the AWS cloud account
+	:type credentials: CloudAccountCredentials
+	:param organizational_unit_id:
+	:type organizational_unit_id: str
 
 	"""
 
 	name: str
-	credentials: AwsCloudAccountCredentials
+	credentials: CloudAccountCredentials
 	organizational_unit_id: str = None
 
 
 @dataclass
-class AwsCloudAccountNetSecRegion:
+class CloudAccountRegionConfiguration:
 	"""AWS cloud account net sec region
 
-		Args:
-			region (str): (Required) AWS region, in AWS format (e.g., "us-east-1")
-			new_group_behavior (str): (Required) The network security configuration. Select "ReadOnly", "FullManage", or "Reset".
+	:link https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountregionconfigurationviewmodel
+	:param region: Dome9 representation value for the AWS region
+	:type region: str
+	:param new_group_behavior: The Protection Mode that Dome9 will apply to new security groups detected in the cloud account. ReadOnly New Security Groups will be included in Dome9 in Read-Only mode, without changes to any of the rules FullManage New Security Groups will be included in Dome9 in Full Protection mode, without changes to any of the rules Reset New Security Groups will be included in Dome9 in Full Protection mode, and all inbound and outbound rules will be cleared
+	:type new_group_behavior: str
+
 
 	"""
 	region: str
@@ -86,9 +93,11 @@ class AwsCloudAccountNetSecRegion:
 class AwsCloudAccountUpdateName(BaseDataclassRequest):
 	"""AWS cloud account update name
 
-		Args:
-			cloud_account_id (str): (Required) AWS cloud account id
-			data (str): (Required) The desired name for aws cloud account
+	:link  https://api-v2-docs.dome9.com/#cloudaccounts_updatecloudaccountname
+	:param cloud_account_id:
+	:type  cloud_account_id: str
+	:param data:the new name for the account
+	:type  data: str
 
 	"""
 	cloud_account_id: str
@@ -96,40 +105,45 @@ class AwsCloudAccountUpdateName(BaseDataclassRequest):
 
 
 @dataclass
-class AwsCloudAccountUpdateConfig(BaseDataclassRequest):
+class CloudAccountRegionConfigurationViewModel(BaseDataclassRequest):
 	"""AWS cloud account update config
 
-		Args:
-			cloud_account_id (str): (Required) AWS cloud account id
-			data (AwsCloudAccountNetSecRegion): (Required) AWS cloud account net sec region
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountpartialupdateviewmodel_dome9-web-api-models-cloudaccountregionconfigurationviewmodel_
+	:param cloud_account_id: The Dome9 cloud account id, at least one of the following properties must be provided: "cloudAccountId", "externalAccountNumber"
+	:type  cloud_account_id: str
+	:param data:
+	:type  data: CloudAccountRegionConfiguration
 
 	"""
 	cloud_account_id: str
-	data: AwsCloudAccountNetSecRegion
+	data: CloudAccountRegionConfiguration
 
 
 @dataclass
-class AwsCloudAccountUpdateOrganizationalUnitID(BaseDataclassRequest):
+class CloudAccountUpdateOrganizationalUnitId(BaseDataclassRequest):
 	"""AWS cloud account update organizational unit id
 
-		Args:
-			organizational_unit_id (str): (Required) The desired organizational unit id to attach to
+	:link https://api-v2-docs.dome9.com/#cloudaccounts_updateorganziationalid
+	:param organizational_unit_id: The Guid ID of the Organizational Unit to attach to. Use 'null' to attach to the root Organizational Unit
+	:type  organizational_unit_id: str
 
 	"""
 	organizational_unit_id: str
 
 
 @dataclass
-class AwsCloudAccountUpdateCredentials(BaseDataclassRequest):
+class CloudAccountCredentialsViewModel(BaseDataclassRequest):
 	"""AWS cloud account update credentials
 
-		Args:
-			organizational_unit_id (str): (Required) (Required) AWS cloud account id
-			data (str): (Required) AWS cloud account credentials
+	:link https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountpartialupdateviewmodel_dome9-web-api-models-cloudaccountcredentialsviewmodel_
+	:param cloud_account_id: The Dome9 cloud account id
+	:type  cloud_account_id: str
+	:param data:
+	:type  data: CloudAccountCredentials
 
 	"""
 	cloud_account_id: str
-	data: AwsCloudAccountCredentials
+	data: CloudAccountCredentials
 
 
 class AwsCloudAccount(Dome9Resource):
@@ -137,92 +151,93 @@ class AwsCloudAccount(Dome9Resource):
 	def __init__(self, client: Client):
 		super().__init__(client)
 
-	def create(self, body: AwsCloudAccountRequest) -> Dict:
+	def create(self, body: CloudAccount) -> Dict:
 		"""Create (onboard) aws cloud account
 
-		:param body: Details for the new aws cloud account
-		:type body: AwsCloudAccountRequest
-		:returns: Dict that has metadata for the created aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_post
+		:param   body: Details for the new aws cloud account
+		:type    body: CloudAccount
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype   CloudAccount
 
 		"""
-		return self._post(route=AwsCloudAccountConsts.MAIN_ROUTE.value, body=body)
+		return self._post(route=AwsCloudAccountConsts.CLOUD_ACCOUNTS.value, body=body)
 
-	def get(self, aws_cloud_account_id: str) -> Dict:
-		"""Get aws cloud account that onboarded to dome9
+	def get(self, aws_cloud_account_id: str = '') -> Union[Dict, List[Dict]]:
+		"""Fetch a specific AWS cloud account
 
-		:param aws_cloud_account_id: Dome9 aws cloud account id
-		:type aws_cloud_account_id: str
-		:returns: Dict that has metadata for aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_get
+		:param   aws_cloud_account_id: Dome9 aws cloud account id
+		:type    aws_cloud_account_id: str
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype    CloudAccount
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{aws_cloud_account_id}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{aws_cloud_account_id}'
 		return self._get(route=route)
 
-	def get_all(self) -> List[Dict]:
-		"""Get all aws cloud accounts that already onboarded to dome9
+	def update_cloud_account_name(self, body: AwsCloudAccountUpdateName) -> Dict:
+		"""Update an AWS cloud account name
 
-		:returns: List of dicts that has metadata for all the aws cloud accounts
-
-		"""
-		return self._get(route=AwsCloudAccountConsts.MAIN_ROUTE.value)
-
-	def update_name(self, body: AwsCloudAccountUpdateName) -> Dict:
-		"""Update the name for aws cloud accounts that already onboarded to dome9
-
-		:param body: Details for dome9 aws cloud account
-		:type body: AwsCloudAccountUpdateName
-
-		:returns: Dict that has metadata for aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_updatecloudaccountname
+		:param   body: Details for dome9 aws cloud account
+		:type    body: AwsCloudAccountUpdateName
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype   CloudAccount
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{AwsCloudAccountConsts.NAME_ROUTE.value}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{AwsCloudAccountConsts.NAME.value}'
 		return self._put(route=route, body=body)
 
-	def update_region_config(self, body: AwsCloudAccountUpdateConfig) -> Dict:
-		"""Update the region config for aws cloud accounts that already onboarded to dome9
+	def update_region_config(self, body: CloudAccountRegionConfigurationViewModel) -> Dict:
+		"""Update an AWS cloud account region configuration
 
-		:param body: Details for updating the region config of the dome9 aws cloud account
-		:type body: AwsCloudAccountUpdateConfig
-
-		:returns: Dict that has metadata for aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_updatecloudaccountregionconf
+		:param   body: updated Regional Configuration parameters for the account
+		:type    body: CloudAccountRegionConfigurationViewModel
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype   CloudAccount
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{AwsCloudAccountConsts.REGION_CONFIG_ROUTE.value}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{AwsCloudAccountConsts.REGION_CONFIG.value}'
 		return self._put(route=route, body=body)
 
-	def update_organizational_id(self, aws_cloud_account_id: str, body: AwsCloudAccountUpdateOrganizationalUnitID) -> Dict:
-		"""Update the organizational unit id for aws cloud accounts that already onboarded to dome9
+	def update_organizational_id(self, aws_cloud_account_id: str, body: CloudAccountUpdateOrganizationalUnitId) -> Dict:
+		"""Update the ID of the Organizational Unit that this cloud account will be attached to. Use 'null' for the root Organizational Unit
 
-		:param aws_cloud_account_id: Dome9 aws cloud account id
-		:type aws_cloud_account_id: str
-		:param body: Details for aws cloud account in order to update organizational unit id.
-		:type body: AwsCloudAccountUpdateOrganizationalUnitID
-
-		:returns: Dict that has metadata for aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_updateorganziationalid
+		:param   aws_cloud_account_id: The Dome9 Guid ID of the AWS cloud account
+		:type    aws_cloud_account_id: str
+		:param   body: The Guid ID of the Organizational Unit to attach to. Use 'null' to attach to the root Organizational Unit
+		:type    body: CloudAccountUpdateOrganizationalUnitId
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype   CloudAccount
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{aws_cloud_account_id}/{AwsCloudAccountConsts.ORGANIZATIONAL_UNIT_ROUTE.value}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{aws_cloud_account_id}/{AwsCloudAccountConsts.ORGANIZATIONAL_UNIT.value}'
 		return self._put(route=route, body=body)
 
-	def update_credentials(self, body: AwsCloudAccountUpdateCredentials) -> Dict:
-		"""Update the credentials for aws cloud accounts that already onboarded to dome9
+	def update_credentials(self, body: CloudAccountCredentialsViewModel) -> Dict:
+		"""Update credentials for an AWS cloud account in Dome9. At least one of the following properties must be provided: "cloudAccountId", "externalAccountNumber"
 
-		:param body: Details for aws cloud account in order to update the credentials.
-		:type body: AwsCloudAccountUpdateCredentials
-
-		:returns: Dict that has metadata for aws cloud account
+		:link    https://api-v2-docs.dome9.com/#cloudaccounts_updatecloudaccountcredentials
+		:param   body: credentials block
+		:type    body: CloudAccountCredentialsViewModel
+		:returns https://api-v2-docs.dome9.com/#schemadome9-web-api-models-cloudaccountviewmodel
+		:rtype   CloudAccount
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{AwsCloudAccountConsts.CREDENTIALS_ROUTE.value}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{AwsCloudAccountConsts.CREDENTIALS.value}'
 		return self._put(route=route, body=body)
 
 	def delete(self, aws_cloud_account_id: str):
-		"""Delete aws cloud account
+		"""Delete an AWS cloud account
 
-		:param aws_cloud_account_id: Aws cloud account id
+		:link https://api-v2-docs.dome9.com/#cloudaccounts_delete
+		:param aws_cloud_account_id: The Dome9 AWS account id (UUID)
 		:type aws_cloud_account_id: str
 		:returns: None
 
 		"""
-		route = f'{AwsCloudAccountConsts.MAIN_ROUTE.value}/{aws_cloud_account_id}'
+		route = f'{AwsCloudAccountConsts.CLOUD_ACCOUNTS.value}/{aws_cloud_account_id}'
 		return self._delete(route=route)
