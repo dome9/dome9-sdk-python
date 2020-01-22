ae79f70795b775596b3f2c4412e81c926ddbc088
erez-dome9 <erez@dome9.com>
2020-01-22 15:27:26 +0200

    Azure cloud account re-doc (#34)

--- dome9_type_annotations/azure_cloud_account.py
+++ dome9_type_annotations/azure_cloud_account.py
@@ -1,37 +1,103 @@
-from resources.azure_cloud_account import AzureCloudAccount as BaseAzureCloudAccount, AzureCloudAccountUpdateOrganizationalUnitID, AzureCloudAccountUpdateCredentials, \
- AzureCloudAccountUpdateOperationMode, AzureCloudAccountUpdateName, AzureCloudAccountRequest
+from resources.azure_cloud_account import AzureCloudAccount as BaseAzureCloudAccount, AzureCloudAccountUpdateOrganizationalUnitID, AzureCloudAccountCredentialsPut, \
+ AzureAccountOperationMode, AzureAccountNameMode, AzureCloudAccountRequest
 
 
 class azure_cloud_account(BaseAzureCloudAccount):
 
 	@classmethod
 	def create(cls, body: AzureCloudAccountRequest):
+		"""Add (onboard) an Azure account to the user's Dome9 account
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_post
+		:param   body: details for the Azure account
+		:type    body: AzureCloudAccountRequest
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
-	def get(cls, azure_cloud_account_id: str):
+	def get(cls, azure_cloud_account_id: str = ''):
+		"""Get details for all Azure accounts for the Dome9 user
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_get
+		:param   azure_cloud_account_id:
+		:type    azure_cloud_account_id: str
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
-	def get_all(cls):
+	def update_account_name(cls, azure_cloud_account_id: str, body: AzureAccountNameMode):
+		"""Update the account name (in Dome9) for an Azure account
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateaccountname
+		:param   azure_cloud_account_id: the account id in Dome9
+		:type    azure_cloud_account_id: str
+		:param   body: the new name for the account
+		:type    body: AzureAccountNameMode
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
-	def update_name(cls, azure_cloud_account_id: str, body: AzureCloudAccountUpdateName):
+	def update_operation_mode(cls, azure_cloud_account_id: str, body: AzureAccountOperationMode):
+		"""Update the operations mode for an Azure account in Dome9. Modes can be Read-Only or Manage
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateoperationmodeasync
+		:param   azure_cloud_account_id: the account id for the Azure account in Dome9
+		:type    azure_cloud_account_id: str
+		:param   body: updated details for the account, including the operations mode
+		:type    body: AzureAccountOperationMode
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
-	def update_operation_mode(cls, azure_cloud_account_id: str, body: AzureCloudAccountUpdateOperationMode):
-		pass
+	def update_cloud_account_credentials(cls, azure_cloud_account_id: str, body: AzureCloudAccountCredentialsPut):
+		"""
 
-	@classmethod
-	def update_credentials(cls, azure_cloud_account_id: str, body: AzureCloudAccountUpdateCredentials):
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updatecloudaccountcredentials
+		:param   azure_cloud_account_id:
+		:type    azure_cloud_account_id: str
+		:param   body:
+		:type    body: AzureCloudAccountCredentialsPut
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
 	def update_organizational_id(cls, azure_cloud_account_id: str, body: AzureCloudAccountUpdateOrganizationalUnitID):
+		"""Update the ID of the Organizational Unit that this cloud account will be attached to. Use 'null' for th root organizational unit
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateorganziationalid
+		:param   azure_cloud_account_id: The Dome9 Guid ID of the Azure cloud account
+		:type    azure_cloud_account_id: str
+		:param   body: The Guid ID of the Organizational Unit to attach to. Use 'null' to attach to the root Organizational Unit
+		:type    body: AzureCloudAccountUpdateOrganizationalUnitID
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
 		pass
 
 	@classmethod
 	def delete(cls, azure_cloud_account_id: str):
+		"""Delete an Azure account from a Dome9 account (the Azure account is not deleted from Azure)
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_delete
+		:param   azure_cloud_account_id: the Dome9 account id for the account
+		:type    azure_cloud_account_id: str
+		:return: None
+
+		"""
 		pass
--- resources/azure_cloud_account.py
+++ resources/azure_cloud_account.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Dict, List, Union
 
 from loguru import logger
 from dome9 import Client, BaseDataclassRequest, Dome9Resource
 
 
 class AzureCloudAccountConsts(Enum):
-	MAIN_ROUTE = 'AzureCloudAccount'
-	OPERATION_MODE_ROUTE = 'OperationMode'
-	ORGANIZATIONAL_UNIT_ROUTE = 'organizationalUnit'
-	ACCOUNT_NAME_ROUTE = 'AccountName'
-	CREDENTIALS_ROUTE = 'credentials'
+	AZURE_CLOUD_ACCOUNT = 'AzureCloudAccount'
+	OPERATION_MODE = 'OperationMode'
+	ORGANIZATIONAL_UNIT = 'organizationalUnit'
+	ACCOUNT_NAME = 'AccountName'
+	CREDENTIALS = 'credentials'
 
 
 class AzureCloudAccountOperationModeConsts(Enum):
@@ -19,15 +20,16 @@ class AzureCloudAccountOperationModeConsts(Enum):
 
 
 @dataclass
-class AzureCloudAccountCredentials(BaseDataclassRequest):
+class AzureAccountCredentials(BaseDataclassRequest):
 	"""Azure cloud account credentials
 
-		Args:
-			client_id (str): (Required) Azure account id
-			client_password (str): (Required) Password for account
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azureaccountcredentialsviewmodel
+	:param client_id: Azure account id
+	:type  client_id: str
+	:param client_password: Password for account
+	:type  client_password: str
 
 	"""
-
 	client_id: str
 	client_password: str
 
@@ -36,20 +38,25 @@ class AzureCloudAccountCredentials(BaseDataclassRequest):
 class AzureCloudAccountRequest(BaseDataclassRequest):
 	"""Azure cloud account request
 
-		Args:
-			name (str): (Required) The name of the Azure account in Dome9
-			subscription_id (str): (Required) The Azure subscription id for account
-			tenant_id (str): (Required) The Azure tenant id
-			credentials (stAzureCloudAccountCredentials): (Required) Credentials
-			operation_mode (str): (Required) Dome9 operation mode for the Azure account ("Read-Only" or "Managed")
-			organizational_unit_id (str): (Optional) Organizational Unit that this cloud account will be attached to
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+	:param name: account name (in Dome9)
+	:type  name: str
+	:param subscription_id: Azure subscription id for account
+	:type  subscription_id: str
+	:param tenant_id: Azure tenant id
+	:type  tenant_id: str
+	:param credentials: Azure account credentials
+	:type  credentials: AzureAccountCredentials
+	:param operation_mode: Dome9 operation mode for the Azure account (Read-Only or Managed)
+	:type  operation_mode: str
+	:param organizational_unit_id:
+	:type  organizational_unit_id: str
 
 	"""
-
 	name: str
 	subscription_id: str
 	tenant_id: str
-	credentials: AzureCloudAccountCredentials
+	credentials: AzureAccountCredentials
 	operation_mode: str
 	organizational_unit_id: str = None
 
@@ -61,24 +68,26 @@ class AzureCloudAccountRequest(BaseDataclassRequest):
 
 
 @dataclass
-class AzureCloudAccountUpdateName(BaseDataclassRequest):
-	"""Azure cloud account update name
-
-		Args:
-			name (str): (Required) The desired name for Azure cloud account
+class AzureAccountNameMode(BaseDataclassRequest):
 	"""
 
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-azure-accounts-azureaccountnamemodeviewmodel
+	:param name:
+	:type  name: str
+
+	"""
 	name: str
 
 
 @dataclass
-class AzureCloudAccountUpdateOperationMode(BaseDataclassRequest):
-	"""Azure cloud account update name
-
-		Args:
-			operation_mode (str): Dome9 operation mode for the Azure account ("Read-Only" or "Managed")
+class AzureAccountOperationMode(BaseDataclassRequest):
 	"""
 
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-azure-accounts-azureaccountoperationmodeviewmodel
+	:param operation_mode:
+	:type  operation_mode: str
+
+	"""
 	operation_mode: str
 
 	@logger.catch(reraise=True)
@@ -90,24 +99,27 @@ class AzureCloudAccountUpdateOperationMode(BaseDataclassRequest):
 
 @dataclass
 class AzureCloudAccountUpdateOrganizationalUnitID(BaseDataclassRequest):
-	"""Azure cloud account update name
-
-		Args:
-			organizational_unit_id (str): (Required) Organizational Unit ID
 	"""
 
+	:link
+	:param organizational_unit_id:
+	:type  organizational_unit_id: str
+
+	"""
 	organizational_unit_id: str
 
 
 @dataclass
-class AzureCloudAccountUpdateCredentials(BaseDataclassRequest):
-	"""Azure cloud account update credentials
-
-		Args:
-			application_id (str): (Required) Azure application ID
-			application_key (str): (Required) Azure application key
+class AzureCloudAccountCredentialsPut(BaseDataclassRequest):
 	"""
 
+	:link  https://api-v2-docs.dome9.com/#schemadome9-web-api-azure-accounts-azurecloudaccountcredentialsputviewmodel
+	:param application_id:
+	:type  application_id: str
+	:param application_key:
+	:type  application_key: str
+
+	"""
 	application_id: str
 	application_key: str
 
@@ -117,32 +129,99 @@ class AzureCloudAccount(Dome9Resource):
 	def __init__(self, client: Client):
 		super().__init__(client)
 
-	def create(self, body: AzureCloudAccountRequest):
-		return self._post(route=AzureCloudAccountConsts.MAIN_ROUTE.value, body=body)
+	def create(self, body: AzureCloudAccountRequest) -> Dict:
+		"""Add (onboard) an Azure account to the user's Dome9 account
 
-	def get(self, azure_cloud_account_id: str):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}'
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_post
+		:param   body: details for the Azure account
+		:type    body: AzureCloudAccountRequest
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		return self._post(route=AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value, body=body)
+
+	def get(self, azure_cloud_account_id: str = '') -> Union[List[Dict], Dict]:
+		"""Get details for all Azure accounts for the Dome9 user
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_get
+		:param   azure_cloud_account_id:
+		:type    azure_cloud_account_id: str
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}'
 		return self._get(route=route)
 
-	def get_all(self):
-		return self._get(route=AzureCloudAccountConsts.MAIN_ROUTE.value)
+	def update_account_name(self, azure_cloud_account_id: str, body: AzureAccountNameMode) -> Dict:
+		"""Update the account name (in Dome9) for an Azure account
 
-	def update_name(self, azure_cloud_account_id: str, body: AzureCloudAccountUpdateName):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.ACCOUNT_NAME_ROUTE.value}'
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateaccountname
+		:param   azure_cloud_account_id: the account id in Dome9
+		:type    azure_cloud_account_id: str
+		:param   body: the new name for the account
+		:type    body: AzureAccountNameMode
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.ACCOUNT_NAME.value}'
 		return self._put(route=route, body=body)
 
-	def update_operation_mode(self, azure_cloud_account_id: str, body: AzureCloudAccountUpdateOperationMode):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.OPERATION_MODE_ROUTE.value}'
+	def update_operation_mode(self, azure_cloud_account_id: str, body: AzureAccountOperationMode) -> Dict:
+		"""Update the operations mode for an Azure account in Dome9. Modes can be Read-Only or Manage
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateoperationmodeasync
+		:param   azure_cloud_account_id: the account id for the Azure account in Dome9
+		:type    azure_cloud_account_id: str
+		:param   body: updated details for the account, including the operations mode
+		:type    body: AzureAccountOperationMode
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.OPERATION_MODE.value}'
 		return self._put(route=route, body=body)
 
-	def update_credentials(self, azure_cloud_account_id: str, body: AzureCloudAccountUpdateCredentials):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.CREDENTIALS_ROUTE.value}'
+	def update_cloud_account_credentials(self, azure_cloud_account_id: str, body: AzureCloudAccountCredentialsPut) -> Dict:
+		"""
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updatecloudaccountcredentials
+		:param   azure_cloud_account_id:
+		:type    azure_cloud_account_id: str
+		:param   body:
+		:type    body: AzureCloudAccountCredentialsPut
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.CREDENTIALS.value}'
 		return self._put(route=route, body=body)
 
-	def update_organizational_id(self, azure_cloud_account_id: str, body: AzureCloudAccountUpdateOrganizationalUnitID):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.ORGANIZATIONAL_UNIT_ROUTE.value}'
+	def update_organizational_id(self, azure_cloud_account_id: str, body: AzureCloudAccountUpdateOrganizationalUnitID) -> Dict:
+		"""Update the ID of the Organizational Unit that this cloud account will be attached to. Use 'null' for th root organizational unit
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_updateorganziationalid
+		:param   azure_cloud_account_id: The Dome9 Guid ID of the Azure cloud account
+		:type    azure_cloud_account_id: str
+		:param   body: The Guid ID of the Organizational Unit to attach to. Use 'null' to attach to the root Organizational Unit
+		:type    body: AzureCloudAccountUpdateOrganizationalUnitID
+		:return: https://api-v2-docs.dome9.com/#schemadome9-web-api-models-azurecloudaccountviewmodel
+		:rtype   AzureCloudAccountRequest
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}/{AzureCloudAccountConsts.ORGANIZATIONAL_UNIT.value}'
 		return self._put(route=route, body=body)
 
-	def delete(self, azure_cloud_account_id: str):
-		route = f'{AzureCloudAccountConsts.MAIN_ROUTE.value}/{azure_cloud_account_id}'
+	def delete(self, azure_cloud_account_id: str) -> None:
+		"""Delete an Azure account from a Dome9 account (the Azure account is not deleted from Azure)
+
+		:link    https://api-v2-docs.dome9.com/#azurecloudaccount_delete
+		:param   azure_cloud_account_id: the Dome9 account id for the account
+		:type    azure_cloud_account_id: str
+		:return: None
+
+		"""
+		route = f'{AzureCloudAccountConsts.AZURE_CLOUD_ACCOUNT.value}/{azure_cloud_account_id}'
 		return self._delete(route=route)
