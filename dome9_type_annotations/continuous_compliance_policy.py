19ea90020e074b980673811ab42fa94860732af4
Ayal <ayalg@checkpoint.com>
2020-01-28 11:34:55 +0200

    Continuous compliance policy re-doc
    - Continuous compliance policy re-doc

--- dome9_type_annotations/continuous_compliance_policy.py
+++ dome9_type_annotations/continuous_compliance_policy.py
@@ -1,27 +1,59 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from resources.continuous_compliance_policy import ContinuousCompliancePolicyRequest
-from resources.continuous_compliance_policy import ContinuousCompliancePolicy as BaseAwsCloudAccount
+from resources.continuous_compliance_policy import ContinuousCompliancePolicy as ContinuousCompliancePolicy
 
 
-class continuous_compliance_policy(BaseAwsCloudAccount):
+class continuous_compliance_policy(ContinuousCompliancePolicy):
 
 	@classmethod
 	def create(cls, body: ContinuousCompliancePolicyRequest) -> Dict:
+		"""Create continuous compliance policy
+
+		:link   https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_post
+		:param  body: Details for the new continuous compliance policy
+		:type   body: ContinuousCompliancePolicyRequest
+		:return https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype  ContinuousCompliancePolicyGet
+
+		"""
 		pass
 
 	@classmethod
-	def get(cls, continuous_compliance_policy_id: str) -> Dict:
-		pass
+	def get(cls, continuous_compliance_policy_id: str = '') -> Union[Dict, List[Dict]]:
+		"""Get information for continuous compliance policy
 
-	@classmethod
-	def get_all(cls) -> List[Dict]:
+		:link    https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_get
+		:param   continuous_compliance_policy_id: Continuous compliance policy id
+		:type    continuous_compliance_policy_id: str
+		:returns https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype   ContinuousCompliancePolicyGet
+
+		"""
 		pass
 
 	@classmethod
 	def update(cls, continuous_compliance_policy_id: str, body: ContinuousCompliancePolicyRequest) -> Dict:
+		"""Update continuous compliance policy
+
+		:link   https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_put
+		:param  body: Details for continuous compliance policy
+		:type   body: ContinuousCompliancePolicyRequest
+		:param  continuous_compliance_policy_id: Continuous compliance policy id
+		:type   continuous_compliance_policy_id: str
+		:return https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype  ContinuousCompliancePolicyGet
+
+		"""
 		pass
 
 	@classmethod
 	def delete(cls, continuous_compliance_policy_id: str) -> None:
+		"""Delete continuous compliance policy
+
+		:param  continuous_compliance_policy_id: Continuous compliance policy id
+		:type   continuous_compliance_policy_id: str
+		:return None
+
+		"""
 		pass
--- resources/continuous_compliance_policy.py
+++ resources/continuous_compliance_policy.py
@@ -1,24 +1,29 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from dome9 import Client, Dome9Resource, BaseDataclassRequest
 
 
 class ContinuousCompliancePolicyConsts(Enum):
-	MAIN_ROUTE = 'Compliance/ContinuousCompliancePolicy'
+	CONTINUOUS_COMPLIANCE_POLICY = 'Compliance/ContinuousCompliancePolicy'
 
 
 @dataclass
 class ContinuousCompliancePolicyRequest(BaseDataclassRequest):
 	"""AWS security group request
 
-		Args:
-			cloud_account_id (str): Cloud account id
-			external_account_id (str): External account id
-			cloud_account_type (str): The cloud account provider ("AWS", "Azure", "Google")
-			bundle_id (str): The bundle id for the bundle that will be used in the policy
-			notification_ids (List[str]): The notification policy id's for the policy
+	:link: https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicypostviewmodel
+	:param cloud_account_id: Cloud account id
+	:type  cloud_account_id: str
+	:param external_account_id: External account id
+	:type  external_account_id: str
+	:param cloud_account_type: The cloud account provider ("AWS", "Azure", "Google")
+	:type  cloud_account_type: str
+	:param bundle_id: The bundle id for the bundle that will be used in the policy
+	:type  bundle_id: str
+	:param notification_ids: The notification policy id's for the policy
+	:type  notification_ids: List[str]
 
 	"""
 	cloud_account_id: str
@@ -36,53 +41,50 @@ class ContinuousCompliancePolicy(Dome9Resource):
 	def create(self, body: ContinuousCompliancePolicyRequest) -> Dict:
 		"""Create continuous compliance policy
 
-		:param body: Details for the new continuous compliance policy
-		:type body: ContinuousCompliancePolicyRequest
-		:returns: Dict that has metadata for the created continuous compliance policy
+		:link   https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_post
+		:param  body: Details for the new continuous compliance policy
+		:type   body: ContinuousCompliancePolicyRequest
+		:return https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype  ContinuousCompliancePolicyGet
 
 		"""
-		return self._post(route=ContinuousCompliancePolicyConsts.MAIN_ROUTE.value, body=body)
+		return self._post(route=ContinuousCompliancePolicyConsts.CONTINUOUS_COMPLIANCE_POLICY.value, body=body)
 
-	def get(self, continuous_compliance_policy_id: str) -> Dict:
+	def get(self, continuous_compliance_policy_id: str = '') -> Union[Dict, List[Dict]]:
 		"""Get information for continuous compliance policy
 
-		:param continuous_compliance_policy_id: Continuous compliance policy id
-		:type continuous_compliance_policy_id: str
-		:returns: Dict that has metadata for continuous compliance policy
+		:link    https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_get
+		:param   continuous_compliance_policy_id: Continuous compliance policy id
+		:type    continuous_compliance_policy_id: str
+		:returns https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype   ContinuousCompliancePolicyGet
 
 		"""
-		route = f'{ContinuousCompliancePolicyConsts.MAIN_ROUTE.value}/{continuous_compliance_policy_id}'
+		route = f'{ContinuousCompliancePolicyConsts.CONTINUOUS_COMPLIANCE_POLICY.value}/{continuous_compliance_policy_id}'
 		return self._get(route=route)
-
-	def get_all(self) -> List[Dict]:
-		"""Get all continuous compliance policies
-
-		:returns: List of dicts that has metadata for all the continuous compliance policies
-
-		"""
-		return self._get(route=ContinuousCompliancePolicyConsts.MAIN_ROUTE.value)
 
 	def update(self, continuous_compliance_policy_id: str, body: ContinuousCompliancePolicyRequest) -> Dict:
 		"""Update continuous compliance policy
 
-		:param body: Details for continuous compliance policy
-		:type body: ContinuousCompliancePolicyRequest
-		:param continuous_compliance_policy_id: Continuous compliance policy id
-		:type continuous_compliance_policy_id: str
-
-		:returns: Dict that has metadata for updated continuous compliance policy
+		:link   https://api-v2-docs.dome9.com/index.html?python#continuouscompliancepolicy_put
+		:param  body: Details for continuous compliance policy
+		:type   body: ContinuousCompliancePolicyRequest
+		:param  continuous_compliance_policy_id: Continuous compliance policy id
+		:type   continuous_compliance_policy_id: str
+		:return https://api-v2-docs.dome9.com/index.html?python#schemadome9-web-api-compliance-continuouscompliance-continuouscompliancepolicygetviewmodel
+		:rtype  ContinuousCompliancePolicyGet
 
 		"""
-		route = f'{ContinuousCompliancePolicyConsts.MAIN_ROUTE.value}/{continuous_compliance_policy_id}'
+		route = f'{ContinuousCompliancePolicyConsts.CONTINUOUS_COMPLIANCE_POLICY.value}/{continuous_compliance_policy_id}'
 		return self._put(route=route, body=body)
 
 	def delete(self, continuous_compliance_policy_id: str) -> None:
 		"""Delete continuous compliance policy
 
-		:param continuous_compliance_policy_id: Continuous compliance policy id
-		:type continuous_compliance_policy_id: str
-		:returns: None
+		:param  continuous_compliance_policy_id: Continuous compliance policy id
+		:type   continuous_compliance_policy_id: str
+		:return None
 
 		"""
-		route = f'{ContinuousCompliancePolicyConsts.MAIN_ROUTE.value}/{continuous_compliance_policy_id}'
+		route = f'{ContinuousCompliancePolicyConsts.CONTINUOUS_COMPLIANCE_POLICY.value}/{continuous_compliance_policy_id}'
 		return self._delete(route=route)
