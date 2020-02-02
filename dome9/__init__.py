d6bd63a811a19f1bbe8b0cc17309cb6ea1bdb803
Erez Weinstein <erez@dome9.com>
2020-02-02 16:24:17 +0200

    CR Fix

--- dome9/__init__.py
+++ dome9/__init__.py
@@ -4,6 +4,4 @@ from dome9.exceptions import Dome9APIException
 from dome9.resource import Dome9Resource
 from dome9.utils import APIUtils, Utils
 
-__version__ = '1.0'
-
 __all__ = ['BaseDataclassRequest', 'Client', 'Dome9APIException', 'Dome9Resource', 'APIUtils', 'Utils']
