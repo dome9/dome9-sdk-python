eefcd048af6a3979544abd8b188259f0273a24d2
erez-dome9 <erez@dome9.com>
2020-02-02 17:06:43 +0200

    Requirements file  (#44)
    - Project requirements lays in a separate txt file as agreed across Python packages standards

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from dome9 import __version__
 
 
 def readme():
@@ -7,10 +6,18 @@ def readme():
 		return md.read()
 
 
+def requirements():
+	with open('requirements.txt') as requirements_file:
+		return requirements_file.read().splitlines()
+
+def version():
+	with open('VERSION') as version_file:
+		return version_file.read().strip()
+
 setup(name='dome9',
 	description='dome9 py sdk module',
 	python_requires='>3.7',
-	version=__version__,
+	version=version(),
 	long_description=readme(),
 	author='dome9 sre team',
 	author_email='d9ops@checkpoint.com',
@@ -18,10 +25,10 @@ setup(name='dome9',
 	url='git+https://github.com/Dome9/dome9-sdk-python',
 	packages=find_packages(),
 	include_package_data=True,
-	install_requires=['requests', 'loguru', 'dataclasses-json'],
+	install_requires=requirements(),
 	zip_safe=False)
 project_urls = {
-	"Repository": "https://github.com/Dome9/dome9-sdk-python",
-	"Bug Reports": "https://github.com/Dome9/dome9-sdk-python/issues",
-	"Documentation": "https://github.com/Dome9/dome9-sdk-python",
+	"Repository": "https://github.com/dome9/dome9-sdk-python",
+	"Bug Reports": "https://github.com/dome9/dome9-sdk-python/issues",
+	"Documentation": "https://github.com/dome9/dome9-sdk-python",
 }
