9caa6fd7c94679799c8c4fe04ea57e4b4920c13a
Erez Weinstein <erez@dome9.com>
2020-02-04 11:08:17 +0200

    CR Fix

--- scripts/onboarding_cloud_account.py
+++ scripts/onboarding_cloud_account.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import argparse
 import uuid
-import sys
+from typing import Dict
+
+from loguru import logger
 
 from dome9_type_annotations.client import Client
 from resources.aws_cloud_account import CloudAccount, CloudAccountCredentials, CloudAccountUpdateOrganizationalUnitId
@@ -9,7 +11,7 @@ from resources.azure_cloud_account import AzureCloudAccountRequest, AzureAccount
 from resources.role import UpdateRole, Permissions
 
 
-class OnBoardingCloudAccount(object):
+class OnBoardingCloudAccount:
 
 	VENDOR_TYPES = ['aws', 'azure']
 	AWS_ALLOW_READONLY = [True, False]
@@ -18,52 +20,54 @@ class OnBoardingCloudAccount(object):
 	AZURE_OPERATION_MODE = ['Read', 'Manage']
 	AZURE_SRL = '7'
 
-	def __init__(self, args):
-		self.args = args
+	def __init__(self, args: argparse.Namespace):
+		self.args: Dict = vars(args)
 		self.d9_client = Client(access_id=args.dome9ApiKeyID, secret_key=args.dome9ApiKeySecret)
 
-	def onboarding_new_account(self):
-		if self.args.cloudVendorType == 'aws':
-			credentials = CloudAccountCredentials(arn=self.args.awsRoleArn, secret=self.args.awsRoleExternalID)
-			payload = CloudAccount(name=self.args.dome9CloudAccountName, credentials=credentials)
+	def onboarding_new_account(self) -> str:
+		if self.args['cloudVendorType'] == 'aws':
+			credentials = CloudAccountCredentials(arn=self.args['awsRoleArn'], secret=self.args['awsRoleExternalID'])
+			payload = CloudAccount(name=self.args['dome9CloudAccountName'], credentials=credentials)
 			resp = self.d9_client.aws_cloud_account.create(body=payload)
+
 			return resp['id']
 
-		elif self.args.cloudVendorType == 'azure':
-			credentials = AzureAccountCredentials(client_id=self.args.azureApplicationID, client_password=self.args.azureSecretKey)
-			payload = AzureCloudAccountRequest(name=self.args.dome9CloudAccountName,
-				subscription_id=self.args.azureSubscriptionID,
-				tenant_id=self.args.azureActiveDirectoryID,
+		else:
+			credentials = AzureAccountCredentials(client_id=self.args['azureApplicationID'], client_password=self.args['azureSecretKey'])
+			payload = AzureCloudAccountRequest(name=self.args['dome9CloudAccountName'],
+				subscription_id=self.args['azureSubscriptionID'],
+				tenant_id=self.args['azureActiveDirectoryID'],
 				credentials=credentials,
-				operation_mode=self.args.azureOperationMode)
+				operation_mode=self.args['azureOperationMode'])
 			resp = self.d9_client.azure_cloud_account.create(body=payload)
+
 			return resp['id']
 
-	def attach_oi_to_cloud_account(self, cloud_account_id):
-		if self.args.cloudVendorType == 'aws':
-			payload = CloudAccountUpdateOrganizationalUnitId(organizational_unit_id=self.args.dome9OuID)
+	def attach_oi_to_cloud_account(self, cloud_account_id: str) -> None:
+		if self.args['cloudVendorType'] == 'aws':
+			payload = CloudAccountUpdateOrganizationalUnitId(organizational_unit_id=self.args['dome9OuID'])
 			self.d9_client.aws_cloud_account.update_organizational_id(aws_cloud_account_id=cloud_account_id, body=payload)
 
-		elif self.args.cloudVendorType == 'azure':
-			payload = AzureCloudAccountUpdateOrganizationalUnitID(organizational_unit_id=self.args.dome9OuID)
+		else:
+			payload = AzureCloudAccountUpdateOrganizationalUnitID(organizational_unit_id=self.args['dome9OuID'])
 			self.d9_client.azure_cloud_account.update_organizational_id(azure_cloud_account_id=cloud_account_id, body=payload)
 
-	def main_process(self):
-		print(f'onboarding {self.args.cloudVendorType} account')
+	def main_process(self) -> None:
+		logger.info(f'''onboarding {self.args['cloudVendorType']} account''')
 		cloud_account_id = self.onboarding_new_account()
 
-		if self.args.dome9OuID:
-			print(f'attach cloud_account {cloud_account_id} to OU {self.args.dome9OuID}')
+		if self.args['dome9OuID']:
+			logger.info(f'''attach cloud_account {cloud_account_id} to OU {self.args['dome9OuID']}''')
 			self.attach_oi_to_cloud_account(cloud_account_id=cloud_account_id)
 
-		if self.args.cloudVendorType == 'aws':
+		if self.args['cloudVendorType'] == 'aws':
 			srl = '|'.join([OnBoardingCloudAccount.AWS_SRL, cloud_account_id])
-		elif self.args.cloudVendorType == 'azure':
+		else:
 			srl = '|'.join([OnBoardingCloudAccount.AZURE_SRL, cloud_account_id])
 
-		if self.args.dome9AdminRoleID:
-			print(f'grant admin permission to role {self.args.dome9AdminRoleID}')
-			role_object = self.d9_client.role.get(role_id=self.args.dome9AdminRoleID)
+		if self.args['dome9AdminRoleID']:
+			logger.info(f'''grant admin permission to role {self.args['dome9AdminRoleID']}''')
+			role_object = self.d9_client.role.get(role_id=self.args['dome9AdminRoleID'])
 			permission = role_object['permissions']
 			manage_permission = permission['manage']
 			access_permission = permission['access']
@@ -75,21 +79,21 @@ class OnBoardingCloudAccount(object):
 			view_permission.append(srl)
 			permissions = Permissions(manage=manage_permission, create=create_permission, access=access_permission, view=view_permission)
 			payload = UpdateRole(name=role_object['name'], description='dome9 admin role', permissions=permissions)
-			self.d9_client.role.update(role_id=self.args.dome9AdminRoleID, body=payload)
+			self.d9_client.role.update(role_id=self.args['dome9AdminRoleID'], body=payload)
 
-		if self.args.dome9ViewRoleID:
-			print(f'grant view permission to role {self.args.dome9ViewRoleID}')
-			role_object = self.d9_client.role.get(self.args.dome9ViewRoleID)
+		if self.args['dome9ViewRoleID']:
+			logger.info(f'''grant view permission to role {self.args['dome9ViewRoleID']}''')
+			role_object = self.d9_client.role.get(self.args['dome9ViewRoleID'])
 			permission = role_object['permissions']
 			view_permission = permission['view']
 			view_permission.append(srl)
 			permissions = Permissions(view=view_permission)
 			payload = UpdateRole(name=role_object['name'], description='dome9 view role', permissions=permissions)
-			self.d9_client.role.update(role_id=self.args.dome9AdminRoleID, body=payload)
+			self.d9_client.role.update(role_id=self.args['dome9AdminRoleID'], body=payload)
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='')
+	parser = argparse.ArgumentParser(description='cloud account onboarding parameters')
 	useExample = '''
 	AWS:
 	--dome9ApiKeyID sdfsdfssdf --dome9ApiKeySecret sdfsdfssdf --cloudVendorType aws --awsRoleArn arn:aws:iam::111111111:role/Dome9-Connect --awsRoleExternalID sdfsdfsdff --dome9OuID e21b3e8b-e02f-46df-bd70-8ce65ca8a3a5 --dome9CloudAccountName production --dome9AdminRoleID 118187 --dome9ViewRoleID 118203
@@ -97,48 +101,44 @@ if __name__ == '__main__':
 	Azure:
 	--dome9ApiKeyID ddsfsdfsdf --dome9ApiKeySecret sdfsdfssdf --cloudVendorType azure --azureSubscriptionID sdfsdfsdfsdfsd --azureActiveDirectoryID sdfsdsdfsdsdfsd --azureApplicationID sfsdfsdfsfdsdf --azureSecretKey sdfsfsfsfd --dome9OuID 92f9a334-bf29-48a5-9cf8-66a10efe51e6 --dome9CloudAccountName production --dome9AdminRoleID 118881 --dome9ViewRoleID 118901 --azureOperationMode Manage
 	'''
-	parser.epilog = 'Example of use: {} {}'.format(__file__, useExample)
-	parser.add_argument('--dome9ApiKeyID', required=True, type=str, help='(required) Dome9 Api key')
-	parser.add_argument('--dome9ApiKeySecret', required=True, type=str, help='(required) Dome9 secret key')
+	parser.epilog = f'Example of use: {__file__} {useExample}'
+	parser.add_argument('--dome9ApiKeyID', required=True, type=str, help='dome9 api key')
+	parser.add_argument('--dome9ApiKeySecret', required=True, type=str, help='dome9 secret key')
 	parser.add_argument('--cloudVendorType',
 		required=True,
 		type=str,
 		choices=OnBoardingCloudAccount.VENDOR_TYPES,
-		help='(required) type of cloud account vendor: aws, azure')
-	parser.add_argument('--awsRoleArn', required=True, type=str, help='(required) The ARN of the Dome9-Connect role in your AWS account')
+		help='type of cloud account vendor: aws, azure')
+	parser.add_argument('--awsRoleArn', required=True, type=str, help='the arn of the dome9-connect role in your aws account')
 	parser.add_argument('--awsRoleExternalID',
 		required=True,
 		type=str,
-		help='(required) The external ID value used to create the role in your AWS account')
+		help='the external id value used to create the role in your aws account')
 	parser.add_argument('--awsAllowReadOnly',
-		required=False,
 		action='store_true',
-		help='switch parameter, use it for Read-Only, don\'t use it for Full Protection')
+		help='switch parameter, use it for read-only, don\'t use it for full protection')
 	parser.add_argument(
 		'--awsFullProtection',
-		required=False,
 		action='store_true',
 		help=
-		'switch parameter, use it for to set the Security Groups in the account to Full-Protection in the course of onboarding, or don\'t use it to leave them unchanged'
+		'switch parameter, use it for to set the security groups in the account to full-protection in the course of onboarding, or don\'t use it to leave them unchanged'
 	)
-	parser.add_argument('--azureSubscriptionID', required=False, type=str, help='Azure subscriptionID')
-	parser.add_argument('--azureActiveDirectoryID', required=False, type=str, help='Azure azureActiveDirectoryID\\tenantID')
-	parser.add_argument('--azureApplicationID', required=False, type=str, help='Azure azureApplicationID\clientID')
-	parser.add_argument('--azureSecretKey', required=False, type=str, help='Azure azureSecretKey\clientPassword')
+	parser.add_argument('--azureSubscriptionID', type=str, help='azure subscriptionid')
+	parser.add_argument('--azureActiveDirectoryID', type=str, help='azure azureactivedirectoryid\\tenantid')
+	parser.add_argument('--azureApplicationID', type=str, help='azure azureapplicationid\clientid')
+	parser.add_argument('--azureSecretKey', type=str, help='azure azuresecretkey\clientpassword')
 	parser.add_argument('--azureOperationMode',
-		required=False,
 		type=str,
 		default='Read',
 		choices=OnBoardingCloudAccount.AZURE_OPERATION_MODE,
-		help='Default=Read, Azure operationMode, allow Read or Manage')
+		help='default=read, azure operationmode, allow read or manage')
 	parser.add_argument('--dome9CloudAccountName',
-		required=False,
 		type=str,
 		default='account-{}'.format(str(uuid.uuid4())[:8]),
-		help='Default=account-randomString , accountName display on Dome9 console')
-	parser.add_argument('--dome9OuID', required=False, type=str, help='Organization Unit ID to attach cloud account')
-	parser.add_argument('--dome9AdminRoleID', required=False, type=str, help='Dome9 role ID to get admin permission to the account')
-	parser.add_argument('--dome9ViewRoleID', required=False, type=str, help='Dome9 role ID to get read permission to the account')
+		help='default=account-randomstring , accountname display on dome9 console')
+	parser.add_argument('--dome9OuID', type=str, help='organization unit id to attach cloud account')
+	parser.add_argument('--dome9AdminRoleID', type=str, help='dome9 role id to get admin permission to the account')
+	parser.add_argument('--dome9ViewRoleID', type=str, help='dome9 role id to get read permission to the account')
 
 	arguments = parser.parse_args()
 	TestD9Api = OnBoardingCloudAccount(arguments)
--- scripts/update_ruleset.py
+++ scripts/update_ruleset.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import argparse
 import json
-import uuid
+from typing import Dict
+
+from loguru import logger
 
 from dome9_type_annotations.client import Client
 from resources.assessment import AssessmentBundleRequest
@@ -10,24 +12,18 @@ from resources.rule_bundle import RuleBundleRequest
 
 class UpdateRuleset(object):
 
-	@staticmethod
-	def get_json(path):
-		with open(path) as jsonFile:
-			return json.load(jsonFile)
-
-	@staticmethod
-	def create_bundle_request(requestId, cloudAccountID, bundleID):
-		return {'id': bundleID, 'cloudAccountId': cloudAccountID, 'requestId': requestId}
-
-	def __init__(self, args):
-		self.args = args
+	def __init__(self, args: argparse.Namespace):
+		self.args: Dict = vars(args)
 		self.d9_client = Client(access_id=args.dome9ApiKeyID, secret_key=args.dome9ApiKeySecret)
 
-	def update_bundle(self):
-		print(f'loading bundle from {self.args.rulesetJsonPath}')
-		rule_bundle_object = self.get_json(self.args.rulesetJsonPath)
-		print(f'updating bundle {self.args.bundleID}')
-		payload = RuleBundleRequest(id=self.args.bundleID,
+	def update_bundle(self) -> None:
+		logger.info(f'''loading bundle from {self.args['rulesetJsonPath']}''')
+
+		with open(self.args['rulesetJsonPath']) as jsonFile:
+			rule_bundle_object = json.load(jsonFile)
+
+		logger.info(f'''updating bundle {self.args['bundleID']}''')
+		payload = RuleBundleRequest(id=self.args['bundleID'],
 			name=rule_bundle_object['name'],
 			cloud_vendor=rule_bundle_object['cloudVendor'],
 			description=rule_bundle_object['description'],
@@ -35,14 +31,13 @@ class UpdateRuleset(object):
 			language=rule_bundle_object['language'])
 		self.d9_client.rule_bundle.update(body=payload)
 
-	def run_assessment(self):
-		bundle = AssessmentBundleRequest(id=self.args.bundleID, cloud_account_id=self.args.cloudAccountID)
-		print(f'running assessment bundle id {self.args.bundleID}')
+	def run_assessment(self) -> None:
+		bundle = AssessmentBundleRequest(id=self.args['bundleID'], cloud_account_id=self.args['cloudAccountID'])
+		logger.info(f'''running assessment bundle id {self.args['bundleID']}''')
 		bundle_result = self.d9_client.assessment.run_bundle(body=bundle)
-		bundle_result_str = json.dumps(bundle_result)
-		print('bundle result is\n{}'.format(bundle_result_str))
+		logger.info(f'bundle result is: {json.dumps(bundle_result)}')
 
-	def main(self):
+	def main(self) -> None:
 		self.update_bundle()
 		self.run_assessment()
 
@@ -52,13 +47,13 @@ if __name__ == '__main__':
 		description='Given the bundleId and file with ruleset in Json format, updates the bundle and runs assessment')
 
 	useExample = '--dome9ApiKeyID 111111-2222-3333-3333-3333333 --dome9ApiKeySecret qwerrtyy --rulesetJsonPath ruleset.json --bundleID 11111 --cloudAccountID 11111111111'
-	parser.epilog = 'Example of use: {} {}'.format(__file__, useExample)
+	parser.epilog = f'Example of use: {__file__} {useExample}'
 
-	parser.add_argument('--dome9ApiKeyID', required=True, type=str, help='(required) Dome9 Api key')
-	parser.add_argument('--dome9ApiKeySecret', required=True, type=str, help='(required) Dome9 secret key')
-	parser.add_argument('--bundleID', required=True, type=str, help='(required) Dome9 bundle ID')
-	parser.add_argument('--rulesetJsonPath', required=True, type=str, help='(required) Location for ruleset json file')
-	parser.add_argument('--cloudAccountID', required=True, type=str, help='(required) Dome9 external account ID')
+	parser.add_argument('--dome9ApiKeyID', required=True, type=str, help='dome9 api key')
+	parser.add_argument('--dome9ApiKeySecret', required=True, type=str, help='dome9 secret key')
+	parser.add_argument('--bundleID', required=True, type=str, help='dome9 bundle id')
+	parser.add_argument('--rulesetJsonPath', required=True, type=str, help='location for ruleset json file')
+	parser.add_argument('--cloudAccountID', required=True, type=str, help='dome9 external account id')
 
 	arguments = parser.parse_args()
 	updateRuleset = UpdateRuleset(arguments)
